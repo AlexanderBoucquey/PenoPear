dee2ea7b44c54c60069d2358082105673015811b
Alexander Boucquey <alexander.boucquey@student.kuleuven.be>
2018-02-23 14:41:36 +0100

    aanpassingen in kij

--- FEM.m
+++ FEM.m
@@ -1,5 +1,7 @@
 clear all
 close all
+
+% Constanten
 Dur = 2.8E-10;
 Duz = 1.1E-9;
 Dvr = 2.32E-9;
@@ -8,7 +10,6 @@ R_g = 8.314;
 T = 272.15;
 nuu = 2/100;
 nuv =0.7/100;
-%syms V_mu(T) V_mfv(T) C_uamb(T,nuu) C_vamb(T,nuv)
 V_mu = 2.39E10-4*exp(80200/R_g*(1/293.15-1/T));
 V_fv = 1.61E10-4*exp(56700/R_g*(1/293.15-1/T));
 K_mu = 0.4103;
@@ -18,15 +19,22 @@ r_q = 0.97;
 hu = 7E-7;
 hv = 7.5E-7;
 p_atm = 101300;
+
+% Uitrekenbare constanten
 C_uamb = p_atm*nuu/(R_g*T);
 C_vamb = p_atm*nuv/(R_g*T);
+
+% Mesh
 [p,t] = pear_mesh();
 Ku = zeros(length(p),length(p));
 Kv = zeros(length(p),length(p));
 C = zeros(length(p),length(p));
+
+% Randen
 randn = boundary(p);
 randp = p(randn,:);
 
+% Opstellen matrices
 for m = 1:length(t)
     r=p(t(m,:),1);
     z=p(t(m,:),2);
@@ -47,16 +55,17 @@ end
   R_q([randn(length(randn)) randn(1)]) = R_q([randn(end) randn(1)]) + Rq(r([length(randn) 1]),l(length(randn)));
 
 % Lineaire oplossing
-% TODO: x0 = lineaire oplossing
 C_u0 = (Ku + V_mu/K_mu*C + hu*K_h)\(hu*C_uamb*R_q);
 C_v0 = (Kv + hv*K_h)\(r_q*(V_mu/K_mu)*C*C_u0+hv*C_vamb*R_q);
 x0 = [C_u0;C_v0];
 
+% Plot lineaire oplossing oxide
 figure
-%scatter3(p(:,1),p(:,2),C_u0);
 [xi,yi] = meshgrid(-1:0.05:1, -1:0.05:2);
 zi = griddata(p(:,1),p(:,2),C_u0,xi,yi);
 surf(xi,yi,zi);
+
+% Plot lineaire oplossing dioxide
 figure
 [xi,yi] = meshgrid(-1:0.01:1, -1:0.01:2);
 zi = griddata(p(:,1),p(:,2),C_v0,xi,yi);
--- Kij.m
+++ Kij.m
@@ -2,8 +2,7 @@ function [ K ] = Kij( r,z,Dr,Dz )
 [area,~,b,c] = triangle(r,z);
 K = zeros(3,3);
 for i = 1:3
-   for j = 1:3
-       
+   for j = 1:3       
        K(i,j) = (r(1) + r(2) + r(3))/12/area*(Dr*b(i)*b(j) + Dz*c(i)*c(j));
    end
 end

567b063cd9db34279076e63313684dc826d9fd87
Alexander Boucquey <alexander.boucquey@student.kuleuven.be>
2018-03-02 13:10:46 +0100

    alles

--- newton.m
+++ newton.m
@@ -1,15 +1,15 @@
-function [x,iter] = newton(x0,f,fp)
+function [x,iter] = newton(x0,F,J)
 % newton-raphson algorithm
 N = 100; eps = 1.e-5; % define max. no. iterations and error
 maxval = 10000.0; % define value for divergence
 xx = x0;
 while (N>0)
- xn = xx-f(xx)/fp(xx);
-if abs(f(xn))<eps
+ xn = xx-F(xx)/J(xx);
+if abs(F(xn))<eps
  x=xn;iter=100-N;
  return;
 end;
-if abs(f(xx))>maxval
+if abs(F(xx))>maxval
  disp(['iterations = ',num2str(iter)]);
  error('Solution diverges');
  break;
